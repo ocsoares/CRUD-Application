301c69989a88ded3e50e1584f868c44b0a848ce6
ocsoares <casoares016@gmail.com>
2022-09-12 16:04:37 -0300

    fix to deploy

--- src/config/redisConfig.ts
+++ src/config/redisConfig.ts
@@ -3,12 +3,15 @@ import Redis from 'ioredis'
 import { ConnectionOptions } from 'tls'
 
 const redisClient = new Redis({
-    host: 'localhost' || process.env.REDIS_HOST,
+    host: process.env.REDIS_HOST || 'localhost',
+    port: Number(process.env.REDIS_PORT) || 6379,
     password: undefined || process.env.REDIS_PASS,
-    tls: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false as ConnectionOptions
+    // tls: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false as ConnectionOptions
 })
 
 console.log('REDIS_HOST:', process.env.REDIS_HOST);
+console.log('REDIS_PORT:', process.env.REDIS_PORT);
+console.log('ENV:', process.env);
 console.log('NODE_ENV:', process.env.NODE_ENV);
 
 redisClient.on('connect', () => {
