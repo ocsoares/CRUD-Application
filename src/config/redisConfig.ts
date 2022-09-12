74f382c4832405f685035987bcf2d740386b9550
ocsoares <casoares016@gmail.com>
2022-09-12 15:45:46 -0300

    fix to deploy

--- src/config/redisConfig.ts
+++ src/config/redisConfig.ts
@@ -2,12 +2,14 @@ import 'dotenv/config'
 import Redis from 'ioredis'
 import { ConnectionOptions } from 'tls'
 
-const redisClient = new Redis(process.env.REDIS_URL || 'localhost', {
+const redisClient = new Redis({
+    host: 'localhost' || process.env.REDIS_HOST,
     password: undefined || process.env.REDIS_PASS,
     tls: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false as ConnectionOptions
 })
 
-console.log('REDIS_URL:', process.env.REDIS_URL);
+console.log('REDIS_HOST:', process.env.REDIS_HOST);
+console.log('NODE_ENV:', process.env.NODE_ENV);
 
 redisClient.on('connect', () => {
     if(process.env.NODE_ENV === 'production'){
